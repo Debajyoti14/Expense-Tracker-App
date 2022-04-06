c4dab686623bbcb09e3f007aa6ad59c2722a35c4
Debajyoti <debajyotisaha14@gmail.com>
2022-04-07 00:18:05 +0530

    Project done till top chart

--- lib/Widgets/transaction_list.dart
+++ lib/Widgets/transaction_list.dart
@@ -9,37 +9,58 @@ class userTransactionList extends StatelessWidget {
   userTransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map((transaction) => Card(
-                child: Row(children: <Widget>[
-                  Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text('₹ ${transaction.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
-                              fontSize: 20))),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(transaction.title,
-                            style: TextStyle(
-                                color: Colors.purple,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400)),
-                        Text(DateFormat.yMMMd().format(transaction.date),
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 66, 66, 66),
-                                fontSize: 14))
-                      ])
-                ]),
-              ))
-          .toList(),
-    );
+    return Container(
+        height: 300,
+        child: transactions.isEmpty
+            ? Column(children: <Widget>[
+                Text('No transactions added yet',
+                    style: TextStyle(
+                        fontFamily: 'QuickSand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)),
+                SizedBox(height: 15),
+                Container(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
+              ])
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Row(children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2)),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                              '₹ ${transactions[index].amount.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 20))),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(transactions[index].title,
+                                style: TextStyle(
+                                    color: Colors.purple,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400)),
+                            Text(
+                                DateFormat.yMMMd()
+                                    .format(transactions[index].date),
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 66, 66, 66),
+                                    fontSize: 14))
+                          ])
+                    ]),
+                  );
+                },
+                itemCount: transactions.length,
+              ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@
 import 'package:expense_tracker_app/Models/transaction.dart';
 import 'package:flutter/material.dart';
 
-import 'Widgets/user_transaction.dart';
+import 'Widgets/new_transaction.dart';
+import 'Widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,23 +12,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        fontFamily: 'QuickSand',
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _usertransactions = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Eating Lunch',
+    //   amount: 50.00,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Go to College',
+    //   amount: 30.00,
+    //   date: DateTime.now(),
+    // ),
+  ];
+  void _addNewTransactions(String title, double amount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: amount,
+        date: DateTime.now());
+    setState(() {
+      _usertransactions.add(newTx);
+    });
+  }
+
+  void startAddNewTransactions(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: newTransaction(_addNewTransactions),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => startAddNewTransactions(context))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
             //mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               Container(
@@ -39,7 +89,12 @@ class MyHomePage extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              userTransaction()
-            ]));
+              userTransactionList(_usertransactions)
+            ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => startAddNewTransactions(context)),
+    );
   }
 }
